dd5ea79ea28cfec56a573a0a880331ef957a498f
fmiglior <50951828+fmiglior@users.noreply.github.com>
2019-11-21 10:33:11 -0300

    v1

--- Conversor_Can04_can2010.ino
+++ Conversor_Can04_can2010.ino
@@ -3,33 +3,34 @@
 #include <avr/wdt.h>
 
 
-MCP2515 CAN0(10); // recebe da rede can pino10
-MCP2515 CAN1(9); // envia pro painel pino9
+//variables
+MCP2515 CAN0(10); // Pin10 recive can infos from the car
+MCP2515 CAN1(9); // pin9 send info to the dashboard
 struct can_frame canMsgRcv;
 struct can_frame canMsgSnd;
-struct can_frame canMsgSndT; //envia mensagens da tela pra bsi
-struct can_frame canMsgRcvT; //recebido can da tela
+struct can_frame canMsgSndT; 
+struct can_frame canMsgRcvT; 
 int id; //canMsgRcv.can_id
 int len; //canMsgRcv.can_dlc
 int idT; //canMsgRcvT.can_id
 int lenT; //canMsgRcvT.can_dlc
-unsigned char bnovo; //novo brilho
-int rpm; //valor da rpm
-unsigned char trip; //valor botão trip
-unsigned char botao = 0; //bota troca tela
+unsigned char bnovo; //display brightness
+int rpm; //car RPM valeu
+unsigned char trip; //TRIP button value
+unsigned char botao = 0; //external button to change the dashboard pages
 
 //-----------------------------------
 //AREA DE CONFIGURACOES
-bool shiftlight = true; //para ativar ou desativar shiftlight
-int rpmmax = 5500; //rpm do corte, ajuste aqui
-bool botaobrilho = false; //True para ajustar o brilho pelo botao lateral, false para a BSI ajustar o brilho
-bool botaotela = true; //botao para troca de tela
-bool debug = false; //ativar para monitorar o codigo
+bool shiftlight = true; //activate the "shiftlight" function, change some graphics color to red 
+int rpmmax = 5500; //rpm value to active the shiftlight
+bool botaobrilho = false; //to activate the brightness button on the side of the 3008 display
+bool botaotela = true; //activate a external button to change the display page
+bool debug = false; //debug mode read values on serial.
 //-----------------------------------
 
 
 void setup() {
-wdt_enable(WDTO_8S); //tempo do watchdog, reset automatico em caso de travar o codigo
+wdt_enable(WDTO_8S); //watchdog time set to 8 seconds
 
   SPI.begin();
   CAN0.reset();
@@ -39,27 +40,27 @@ wdt_enable(WDTO_8S); //tempo do watchdog, reset automatico em caso de travar o c
   CAN1.setBitrate(CAN_125KBPS,MCP_8MHZ);
   CAN1.setNormalMode(); 
 
-  pinMode(8, INPUT_PULLUP); //pino do botao
+  pinMode(8, INPUT_PULLUP); //external button to change pages pinout.
   
   //while (!Serial);
   if (debug == true) {
   Serial.begin(115200);  
-  Serial.println("RODANDO EM MODO DEPURAÇÃO!");
+  Serial.println("DEBUG MODE!");
   }
   
 
   
- rpmmax = rpmmax * 0.0312;
+ rpmmax = rpmmax * 0.0312; //Calculate the rpm HEX value to activate the shiftlight
 
 }
 
 void loop() {
-wdt_reset();  
+wdt_reset(); //watchdog counting reset 
   
    if( CAN0.readMessage( & canMsgRcv) == MCP2515::ERROR_OK ){
     id = canMsgRcv.can_id;
     len = canMsgRcv.can_dlc;
-      if(id == 182 && shiftlight == true) { //pega o valor do rpm
+      if(id == 182 && shiftlight == true) { //rpm value from the 0x0B6 can code
       rpm = canMsgRcv.data[0] ;
     }
   
@@ -69,10 +70,10 @@ wdt_reset();
         canMsgSnd.data[0] = canMsgRcv.data[4];
         canMsgSnd.data[1] = canMsgRcv.data[6];
         canMsgSnd.data[2] = canMsgRcv.data[7];
-        canMsgSnd.data[3] = canMsgRcv.data[3];
+        canMsgSnd.data[3] = canMsgRcv.data[3]; //handbrake light need adjust on the code
         canMsgSnd.data[4] = canMsgRcv.data[1];
-        canMsgSnd.data[5] = canMsgRcv.data[0]; //ajustar por causa da luz de combustivel
-        canMsgSnd.data[6] = 0x04;// mantem o painel ligado
+        canMsgSnd.data[5] = canMsgRcv.data[0]; //need to adjuste the code fuel light no working
+        canMsgSnd.data[6] = 0x04; //need to use this value to keep the display ON
         canMsgSnd.data[7] = 0x00;
         canMsgSnd.can_id = 0x128;
         canMsgSnd.can_dlc = 8; 
@@ -80,60 +81,60 @@ wdt_reset();
    
          
      }
-     else if (id == 935) { // Manutençao
-        canMsgSnd.data[0] = canMsgRcv.data[0];
+     else if (id == 935) { // maintenance 
+        canMsgSnd.data[0] = canMsgRcv.data[0]; //display the maintenance key signal
         canMsgSnd.data[1] = canMsgRcv.data[5]; // Value x255 +
         canMsgSnd.data[2] = canMsgRcv.data[6]; // Value x1 = Number of days till maintenance (FF FF if disabled)
         canMsgSnd.data[3] = canMsgRcv.data[3]; // Value x5120 +
         canMsgSnd.data[4] = canMsgRcv.data[4]; // Value x20 = km left till maintenance
-        canMsgSnd.can_id = 0x3E7; // novo ID para manutencao
+        canMsgSnd.can_id = 0x3E7; // new id on can2010
         canMsgSnd.can_dlc = 5;
         CAN1.sendMessage(&canMsgSnd);
         
      }
 
      
-     else if (id == 424 && len == 8) { //ID1A8  cruise control
-        canMsgSnd.data[0] = canMsgRcv.data[1]; //velocidade
-        canMsgSnd.data[1] = canMsgRcv.data[2]; //velocidade
-        canMsgSnd.data[2] = canMsgRcv.data[0];
-        canMsgSnd.data[3] = 0x80; //cruise ou limitador
-        canMsgSnd.data[4] = 0x00;
+     else if (id == 424 && len == 8) { //ID1A8  speed limiter, cruise control and instant trip values
+        canMsgSnd.data[0] = canMsgRcv.data[1]; //speed
+        canMsgSnd.data[1] = canMsgRcv.data[2]; //speed
+        canMsgSnd.data[2] = canMsgRcv.data[0]; //cruise on pause 0x80, 0x88 on, speed limiter on pause 0x40, 0x48 on
+        canMsgSnd.data[3] = 0x80; 
+        canMsgSnd.data[4] = 0x00; 
         canMsgSnd.data[5] = 0x00; 
         canMsgSnd.data[6] = 0x00;
         canMsgSnd.data[7] = 0x00;
-        canMsgSnd.can_id = 0x228; //novo id
+        canMsgSnd.can_id = 0x228; //new ID
         canMsgSnd.can_dlc = 8;
       CAN1.sendMessage(&canMsgSnd);
-        canMsgSnd.data[0] =  0x00; 
+        canMsgSnd.data[0] = 0x00; 
         canMsgSnd.data[1] = 0x00; 
         canMsgSnd.data[2] = 0x00;
         canMsgSnd.data[3] = 0x00; 
         canMsgSnd.data[4] = 0x00; 
-        canMsgSnd.data[5] = canMsgRcv.data[5];
+        canMsgSnd.data[5] = canMsgRcv.data[5]; 
         canMsgSnd.data[6] = canMsgRcv.data[6];
         canMsgSnd.data[7] = canMsgRcv.data[7];
-        canMsgSnd.can_id = 0x1A8; //trip parcial
+        canMsgSnd.can_id = 0x1A8; //instant trip
         canMsgSnd.can_dlc = 8;
         CAN1.sendMessage(&canMsgSnd);
       
      }
-       else if (id == 360 && len == 8) { //ID168 outras luzes
+       else if (id == 360 && len == 8) { //ID168 other dashboard lights
         canMsgSnd.data[0] = canMsgRcv.data[0]; 
         canMsgSnd.data[1] = canMsgRcv.data[1]; 
         canMsgSnd.data[2] = canMsgRcv.data[5];
         canMsgSnd.data[3] = canMsgRcv.data[3]; 
-        canMsgSnd.data[4] = canMsgRcv.data[5];
-        canMsgSnd.data[5] = canMsgRcv.data[5]; 
+        canMsgSnd.data[4] = canMsgRcv.data[5];// need adjust
+        canMsgSnd.data[5] = canMsgRcv.data[5];// need adjust
         canMsgSnd.data[6] = canMsgRcv.data[6];
         canMsgSnd.data[7] = canMsgRcv.data[7];
         canMsgSnd.can_id = 0x168;   
         canMsgSnd.can_dlc = 8;
       CAN1.sendMessage(&canMsgSnd);
      }
-     else if (id == 608 && len == 8){ //linguagem e regulagem km/l
-        canMsgSnd.data[0] = 0xA1; //esta fixa em PT-BR
-        canMsgSnd.data[1] = 0x1C; //C e KM/L
+     else if (id == 608 && len == 8){ //language and units
+        canMsgSnd.data[0] = 0xA1; //this HEX is for PT-BR, need change to another language
+        canMsgSnd.data[1] = 0x1C; //this HEX is for C e KM/L, need change to use l/100km ou F 
         canMsgSnd.data[2] = 0x97;
         canMsgSnd.data[3] = 0x9B;
         canMsgSnd.data[4] = 0xE0;
@@ -144,16 +145,16 @@ wdt_reset();
         CAN1.sendMessage( & canMsgSnd);      
      }
      
-     else if (id == 54) { //ID036 brilho e chave ligada
+     else if (id == 54) { //ID036 
                       
-         if (botaobrilho == true && (canMsgRcv.data[3] & 0xFF) >= 32) { //ajusta brilho da tela
+         if (botaobrilho == true && (canMsgRcv.data[3] & 0xFF) >= 32) { //use the external button to adjust the brightness
         canMsgSnd.data[3] = (bnovo & 0xFF);  
           }
-        else {
+        else { //use the radio configuration to adjust the brightness
         canMsgSnd.data[3] = canMsgRcv.data[3];
-       }
+          }
      
-        canMsgSnd.data[0] = canMsgRcv.data[0];
+        canMsgSnd.data[0] = canMsgRcv.data[0]; //86 key turned off, 8E key ON
         canMsgSnd.data[1] = canMsgRcv.data[1];
         canMsgSnd.data[2] = canMsgRcv.data[2];
         canMsgSnd.data[4] = canMsgRcv.data[4];
@@ -169,18 +170,18 @@ wdt_reset();
 
    
     
- else {   //final dois ID a receberem De PARA
+ else {   //any other code, sended to the display without changes
  CAN1.sendMessage( & canMsgRcv);
   }
       
-        if (botaotela == true) { //botao para trocar a tela
+        if (botaotela == true) { //external button to change the display page
         if (digitalRead(8) == LOW) {
 
         botao = botao + 1;
         canMsgSnd.data[0] = 0x00;
         canMsgSnd.data[1] = 0x00;
         canMsgSnd.data[2] = 0x00;
-        canMsgSnd.data[3] = (botao & 0xFF);
+        canMsgSnd.data[3] = (botao & 0xFF); //rotary button on the car, increase 1 to select the next page
         canMsgSnd.data[4] = 0x00;
         canMsgSnd.can_id = 0x0A2; 
         canMsgSnd.can_dlc = 5;
@@ -189,19 +190,19 @@ wdt_reset();
         }
         }
   
- //envia a informacao dos outros mostradores e o shiftlight
+ //Shiftligh and personal page dials 
  //VER SE O RADIO MANDA ALGUMA INFO NA CAN 0x2E9 e se manda o 0x1A9
         if (id == 745) { 
         }
         else {
         canMsgSnd.data[0] = 0x01;
          if (shiftlight == true && rpm >= rpmmax){
-        canMsgSnd.data[1] = 0x57; // RPM + cor vermelha
+        canMsgSnd.data[1] = 0x57; // Show the RPM gauge and change the color to RED (shiftlight)
         }
          else {
-        canMsgSnd.data[1] = 0x17; // RPM + cor azul  
+        canMsgSnd.data[1] = 0x17; // show the RPM gauge
         }
-        canMsgSnd.data[2] = 0x30; // mostrador direita radio
+        canMsgSnd.data[2] = 0x30; //radio gauge
         canMsgSnd.can_id = 0x2E9; 
         canMsgSnd.can_dlc = 3;
         CAN1.sendMessage(&canMsgSnd);
@@ -210,17 +211,17 @@ wdt_reset();
 
         
  //------------------------------------
- //le as infos da can0
+ //Read the display can values and sent to the car
  //----------------------------
  if( CAN1.readMessage( & canMsgRcvT) == MCP2515::ERROR_OK ){;
     idT = canMsgRcvT.can_id;
     lenT = canMsgRcvT.can_dlc;
  }
-   if (idT == 535 && lenT == 8)  { //recebe valor brilho botao tela
+   if (idT == 535 && lenT == 8)  { //read the brightness value from the display
    bnovo = (((canMsgRcvT.data[0]) * 0.0625)+32);
    trip = (canMsgRcvT.data[1]);
    }  
-  //Resetar o TRIP pelo botao lateral
+  //To reset the trip holding the side button of the display
  
    if (trip == 130) {     
         canMsgSndT.data[0] = canMsgRcvT.data[0];
@@ -241,15 +242,12 @@ wdt_reset();
   
    
  else{
- //CAN0.sendMessage(&canMsgRcvT); 
+ //CAN0.sendMessage(&canMsgRcvT); //send the dashboard infos to the car
  }
  if (debug == true) {
 Serial.println(rpm);
 
-Serial.print("bnovo: ");
-Serial.println(bnovo);
-Serial.print("botao: ");
-Serial.println(botao);
+
  }
  
 }
